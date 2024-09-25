c6f760748c6dc4d02b82408de99cc5836428de86
Zayd <166023703+povzayd@users.noreply.github.com>
2024-09-25 12:16:13 +0530

    energy.py
    Execute using python energy.py

---
+++ energy.py
@@ -0,0 +1,5 @@
+a=float(input("Enter your mass(kg) :"))
+b=a*9*10000000000000000
+print("You're ",b,"Joules of Energy i.e enough to destroy this universe")
+# I'll surely add more mass options in future 😂
+# Thanks for reading.
